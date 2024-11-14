c3e1f33197cb5862e56dbdadecaa427ee725f4e2
kirann322 <31964054+kirann322@users.noreply.github.com>
2024-11-14 17:25:38 -0500

    Updated Transaction and added unit tests for Transaction

---
+++ src/driver.py
@@ -0,0 +1,8 @@
+from src.elliptic_curve_cryptography.Secp256k1Curve import *
+from src.elliptic_curve_cryptography.DigitalSignature import *
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
